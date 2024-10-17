4b2167d0af10e31992737c29d51005318d3bc1f3
popopocorn <popcorn375@naver.com>
2024-10-17 21:00:22 +0900

    데스폴트/플슬 제외 이펙트 추출 완

--- script/brandish.py
+++ script/brandish.py
@@ -5,31 +5,12 @@ from pico2d import *
 def play_brandish():
     ani_frame = [load_image( "brandish_effect"+str(i)+".png") for i in range(11) ]
     motion_frame = [load_image("brandish" + str(i) + ".png") for i in range(7) ]
-    ani_index = 0
-    motion_index = 0
 
-    ani_time_per_frame = 70  # ani_frame이 70프레임마다 한 번 변경
-    motion_time_per_frame = 130  # motion_frame이 130프레임마다 한 번 변경
+    ani_frame[2].draw(400, 300)
+    motion_frame[2].draw(470, 280)
+    update_canvas()
 
-    for i in range(770):
-        clear_canvas()
-
-        # ani_frame 애니메이션 처리
-        if i % ani_time_per_frame == 0:
-            ani_index = (ani_index + 1) % len(ani_frame)
-        ani_frame[ani_index].draw(400, 300)
-
-        # motion_frame 애니메이션 처리
-        if i % motion_time_per_frame == 0:
-            motion_index = (motion_index + 1) % len(motion_frame)
-        motion_frame[motion_index].draw(400, 300)
-
-        update_canvas()
-        delay(0.0001)
-
-open_canvas()
+open_canvas(800, 600)
 while True:
     play_brandish()
-#모션 잘못 추출했다.. 이펙트는 새거인데 모션은 리마스터 이전...
-#메이플이 용량을 줄이려는지 구 이펙트 삭제중이라 리마스터 이후로 통일해야함
-#모션 다시 구해야함 하...
+#큰일났다 대부분의 스킬이 이펙트/모션 프레임수가 다름 스킬마다 play함수 따로 정의해서 모듈화 하고 임포트 해야할듯
