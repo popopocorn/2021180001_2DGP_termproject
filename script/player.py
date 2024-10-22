88db0c181cba381d280765a3831d099a0c354f59
popopocorn <popcorn375@naver.com>
2024-10-22 21:55:59 +0900

    1

--- script/player.py
+++ script/player.py
@@ -5,6 +5,7 @@ class Player:
         self.player_x=0
         self.player_y=0
         self.walking_motion = [load_image("walk" + str(x) + ".png") for x in range(4)]
+        self.brandish_motion = [load_image("brandish"+str(x)+".png") for x in range(7)]
     def draw(self):
 
         for i in range(4):
@@ -12,7 +13,12 @@ class Player:
             self.walking_motion[i].draw(400, 300)
             delay(0.1)
             update_canvas()
-        
+        delay(1)
+        for i in range(7):
+            clear_canvas()
+            self.brandish_motion[i].draw(400, 300)
+            delay(0.1)
+            update_canvas()
 open_canvas()
 player = Player()
 while True:
