85b87324933db87eea6b8afa3914c3eba02ef5f7
popopocorn <popcorn375@naver.com>
2024-10-25 14:22:40 +0900

    1

--- script/player.py
+++ script/player.py
@@ -1,61 +1,80 @@
 from pico2d import *
+from time import *
 
-
-
-def set_flag():
-    global running, player_flag, frame
-    events = get_events()
-    for event in events:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_KEYDOWN:
-            if event.key == SDLK_RIGHT:
-                player_flag = 'right'
-                frame = 0
-            elif event.key == SDLK_LEFT:
-                player_flag = 'left'
-                frame = 0
-            elif event.key == SDLK_ESCAPE:
+class Player:
+    def __init__(self):
+        self.player_flag='idle'
+        self.player_x=400
+        self.player_y=50
+        self.walk_motion=[load_image("walk" + str(x) + ".png") for x in range(4)]
+        self.idle_motion=[load_image("idle" + str(x) +".png") for x in range(3)]
+        self.direction = 'r'
+    def set_flag(self):
+        global running, frame
+        events = get_events()
+        for event in events:
+            if event.type == SDL_QUIT:
                 running = False
-        elif event.type == SDL_KEYUP:
-            player_flag='idle'
-            frame=0
-def play_run_animation(dir, frame, dx, dy):
-    clear_canvas()
-    ground.draw(400, 300)
-    player_run.clip_draw(frame*64, dir*64, 64, 64, dx, dy, 200, 200)
-    update_canvas()
-def play_idle_animation(frame, x, y):
-    clear_canvas()
-    ground.draw(400, 300)
-    player_idle.clip_draw(frame*64, 256 - 64, 64, 64, x, y, 200, 200)
-    update_canvas()
-
+            elif event.type == SDL_KEYDOWN:
+                if event.key == SDLK_RIGHT:
+                    self.player_flag = 'right'
+                    self.direction = 'r'
+                    frame = 0
+                elif event.key == SDLK_LEFT:
+                    self.player_flag = 'left'
+                    self.direction = 'l'
+                    frame = 0
+                elif event.key == SDLK_ALTERASE:
+                    self.player_flag = 'jump'
+                    frame = 0
+                elif event.key == SDLK_ESCAPE:
+                    running = False
+            elif event.type == SDL_KEYUP:
+                self.player_flag='idle'
+                frame=0
+    def play_run_animation(self, dir, frame):
+        clear_canvas()
+        if(dir == 'r'):
+            self.walk_motion[frame].composite_draw(0, 'h', self.player_x, self.player_y)
+        else:
+            self.walk_motion[frame].draw(self.player_x, self.player_y)
+        update_canvas()
+    def play_idle_animation(self, dir, frame):
+        clear_canvas()
+        if (dir == 'r'):
+            self.idle_motion[frame].composite_draw(0, 'h', self.player_x, self.player_y)
+        else:
+            self.idle_motion[frame].draw(self.player_x, self.player_y)
+        update_canvas()
+    def get_location(self):
+        return self.player_x, self.player_y
+    def move(self, dx):
+        self.player_x+=dx
+    def get_direction(self):
+        return self.direction
+    def get_flag(self):
+        return self.player_flag
 
 running = True
 frame = 0
-open_canvas()
-player_x=800//2
-player_y=600//2
-player_idle = load_image('Sword_Idle_full.png')
-player_run = load_image('Sword_Run_full.png')
-ground = load_image('TUK_GROUND.png')
-player_flag='idle'
+open_canvas(800,600,sync=True)
 
+
+player=Player()
 while running:
-    set_flag()
-    if(player_flag=='idle'):
-        play_idle_animation(frame, player_x, player_y)
-    elif(player_flag=='left'):
-        play_run_animation(2, frame, player_x, player_y)
-        if player_x>32:
-            player_x-=10
-    elif(player_flag=='right'):
-        play_run_animation(1, frame, player_x, player_y)
-        if player_x<768:
-            player_x+=10
-    if (player_flag=='idle'):
-        frame = (frame+1)%5
+    player.set_flag()
+    if(player.get_flag()=='idle'):
+        player.play_idle_animation(player.direction, frame)
+    elif(player.get_flag()=='left'):
+        player.play_run_animation(player.get_direction(), frame)
+        if player.get_location()[0]>32:
+            player.move(-10)
+    elif(player.get_flag()=='right'):
+        player.play_run_animation(player.get_direction(), frame)
+        if player.get_location()[0]<768:
+            player.move(10)
+    if (player.get_flag()=='idle'):
+        frame = (frame+1)%3
     else:
-        frame = (frame+1)%8
+        frame = (frame+1)%4
     delay(0.05)
