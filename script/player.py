07908042d47f9b22729ca97e67a0ae02dc0e4f39
popopocorn <popcorn375@naver.com>
2024-10-25 21:23:18 +0900

    Revert "1"
    This reverts commit 4ec4c55d8c09523149b46cdfb9b33d9089d8db89.

--- script/player.py
+++ script/player.py
@@ -1,61 +1,25 @@
 from pico2d import *
 
+class Player:
+    def __init__(self):
+        self.player_x=0
+        self.player_y=0
+        self.walking_motion = [load_image("walk" + str(x) + ".png") for x in range(4)]
+        self.brandish_motion = [load_image("brandish"+str(x)+".png") for x in range(7)]
+    def draw(self):
 
-
-def set_flag():
-    global running, player_flag, frame
-    events = get_events()
-    for event in events:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_KEYDOWN:
-            if event.key == SDLK_RIGHT:
-                player_flag = 'right'
-                frame = 0
-            elif event.key == SDLK_LEFT:
-                player_flag = 'left'
-                frame = 0
-            elif event.key == SDLK_ESCAPE:
-                running = False
-        elif event.type == SDL_KEYUP:
-            player_flag='idle'
-            frame=0
-def play_run_animation(dir, frame, dx, dy):
-    clear_canvas()
-    ground.draw(400, 300)
-    player_run.clip_draw(frame*64, dir*64, 64, 64, dx, dy, 200, 200)
-    update_canvas()
-def play_idle_animation(frame, x, y):
-    clear_canvas()
-    ground.draw(400, 300)
-    player_idle.clip_draw(frame*64, 256 - 64, 64, 64, x, y, 200, 200)
-    update_canvas()
-
-
-running = True
-frame = 0
+        for i in range(4):
+            clear_canvas()
+            self.walking_motion[i].draw(400, 300)
+            delay(0.1)
+            update_canvas()
+        delay(1)
+        for i in range(7):
+            clear_canvas()
+            self.brandish_motion[i].draw(400, 300)
+            delay(0.1)
+            update_canvas()
 open_canvas()
-player_x=800//2
-player_y=600//2
-player_idle = load_image('Sword_Idle_full.png')
-player_run = load_image('Sword_Run_full.png')
-ground = load_image('TUK_GROUND.png')
-player_flag='idle'
-
-while running:
-    set_flag()
-    if(player_flag=='idle'):
-        play_idle_animation(frame, player_x, player_y)
-    elif(player_flag=='left'):
-        play_run_animation(2, frame, player_x, player_y)
-        if player_x>32:
-            player_x-=10
-    elif(player_flag=='right'):
-        play_run_animation(1, frame, player_x, player_y)
-        if player_x<768:
-            player_x+=10
-    if (player_flag=='idle'):
-        frame = (frame+1)%5
-    else:
-        frame = (frame+1)%8
-    delay(0.05)
+player = Player()
+while True:
+    player.draw()
