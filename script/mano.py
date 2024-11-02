c3cf9a9ae2d2a79a70aff04ff49bcf732dea6f13
popopocorn <popcorn375@naver.com>
2024-11-02 17:44:55 +0900

    player 오류 수정 완료

--- script/mano.py
+++ script/mano.py
@@ -1,10 +1,15 @@
-
+from pico2d import *
 
 
 
 class Mano:
     def __init__(self):
-        pass
+        self.x=600
+        self.y=50
+        self.hp=300
+        self.idle_motion =[load_image("idle"+str(i)+".png") for i in range(6)]
+        self.skill_motion=[load_image("skill"+str(i)+".png") for i in range(10)]
+
     def update(self):
         pass
     def hadle_event(self):
--- script/player.py
+++ script/player.py
@@ -10,7 +10,12 @@ class Walk:
             player.direction = 'r'
             player.player_dx = 5
             player.frame = 0
-
+        elif left_down(e) or right_up(e):
+            player.direction = 'l'
+            player.player_dx = -5
+            player.frame = 0
+    def exit(self):
+        pass
     @staticmethod
     def do(player):
         player.player_x += player.player_dx
@@ -25,14 +30,17 @@ class Walk:
                 player.jump_motion.composite_draw(0, 'h', player.player_x - 25, player.player_y + 5)
         else:
             if not player.player_jump:
-                player.walk_motion[player.frame].draw(player.player_x - 10, player.player_y)
+                player.walk_motion[player.frame].draw(player.player_x - 25, player.player_y)
             else:
                 player.jump_motion.draw(player.player_x + 15, player.player_y + 5)
 
 
 class Idle:
     @staticmethod
-    def enter(player):
+    def enter(player, e):
+        player.frame = 0
+
+    def exit(self):
         pass
 
     @staticmethod
@@ -45,7 +53,7 @@ class Idle:
             if player.direction == 'r':
                 player.idle_motion[player.frame].composite_draw(0, 'h', player.player_x, player.player_y)
             else:
-                player.idle_motion[player.frame].draw(player.player_x - 10, player.player_y)
+                player.idle_motion[player.frame].draw(player.player_x - 20, player.player_y)
         else:
             if player.direction == 'r':
                 player.jump_motion.composite_draw(0, 'h', player.player_x - 25, player.player_y + 5)
@@ -55,21 +63,41 @@ class Idle:
 
 class Player:
     def __init__(self):
+        self.hp=100
         self.gravity = 3
         self.player_jump = False
         self.player_dx = 0
         self.player_dy = 0
-        self.player_x = 400
+        self.player_x = 200
         self.player_y = 50
         self.walk_motion = [load_image("walk" + str(x) + ".png") for x in range(4)]
         self.idle_motion = [load_image("idle" + str(x) + ".png") for x in range(3)]
         self.jump_motion = load_image(("jump.png"))
         self.direction = 'r'
         self.frame = 0
+        self.state_machine=StateMachine(self)
+        self.state_machine.start(Idle)
+        self.state_machine.set_transitions(
+            {
+                Walk: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle},
+                Idle: {right_down: Walk, left_down: Walk, left_up: Walk, right_up: Walk},
+            }
+        )
 
-    def move(self):
-        self.player_x += self.player_dx
-
+    def draw(self):
+        self.state_machine.draw()
+    def update(self):
+        self.state_machine.update()
+        if self.player_jump:
+            self.update_jump()
+    def handle_event(self, event):
+        if event.type == SDL_KEYDOWN and event.key == SDLK_LALT and not self.player_jump:
+            self.player_jump=True
+            self.player_dy=20
+            print(self.player_jump)
+        self.state_machine.add_event(('INPUT', event))
+    def get_player_location(self):
+        return self.player_x
     def update_jump(self):
         if self.player_jump:
             self.player_y += self.player_dy
@@ -79,7 +107,3 @@ class Player:
                 self.player_y = 50
                 self.player_jump = False
                 self.player_dy = 0  # 초기화
-
-
-    def get_running(self):
-        return self.running
